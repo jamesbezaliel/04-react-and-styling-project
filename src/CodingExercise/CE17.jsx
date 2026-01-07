fc468d010d0d1ecea371888eec25f2f4720b79ef
jamesbezaliel <jamesjonathan343@gmail.com>
2026-01-07 21:02:51 +0700

    add checkpoint until middle of lesson 119

---
+++ src/CodingExercise/CE17.jsx
@@ -0,0 +1,25 @@
+// import React from 'react';
+
+// function App() {
+//     const [color, setColor] = React.useState("white");
+    
+//     function handleChangeColor(value) {
+//         setColor(value);
+//     }
+    
+//   return (
+//     <div id="app">
+//       <h1 style={{color}} >CSS is great!</h1>
+//       <menu>
+//         <li>
+//           <button onClick={() => handleChangeColor('green')}>Yes</button>
+//         </li>
+//         <li>
+//           <button onClick={() => handleChangeColor('red')}>No</button>
+//         </li>
+//       </menu>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/CodingExercise/CE18.jsx
@@ -0,0 +1,25 @@
+// import React from 'react';
+
+// function App() {
+//     const [color, setColor] = React.useState();
+    
+//     function handleClickButton(type) {
+//         setColor(type);
+//     }
+    
+//   return (
+//     <div id="app">
+//       <h1 className={color}>CSS is great!</h1>
+//       <menu>
+//         <li>
+//           <button onClick={() => handleClickButton('highlight-green')}>Yes</button>
+//         </li>
+//         <li>
+//           <button onClick={() => handleClickButton('highlight-red')}>No</button>
+//         </li>
+//       </menu>
+//     </div>
+//   );
+// }
+
+// export default App;
