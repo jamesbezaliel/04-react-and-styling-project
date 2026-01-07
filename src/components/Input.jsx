c708c2c2680f56a6822cc64db180a8435b730eaf
jamesbezaliel <jamesjonathan343@gmail.com>
2026-01-07 21:26:33 +0700

    add tailwind and save progress for lesson 119

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -21,8 +21,11 @@ const Input = styled.input`
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `;
 
-export default function InputComponent() {
+export default function InputComponent({ label, invalid, ...props }) {
   return (
-    <></>
+    <p>
+      <Label $invalid={invalid}>{label}</Label>
+      <Input $invalid={invalid} {...props} />
+    </p>
   );
 }
